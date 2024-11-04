936431e93ea5cc96ce40a5b6187cfc42e096aca3
E2ewebserv <e2ewebservice@gmail.com>
2024-11-04 12:57:09 +0530

    p cmd

--- FridgeService.js
+++ FridgeService.js
@@ -5,7 +5,6 @@ import React from "react";
         
         <div>
           <h1>Welcome to Fridge Service</h1>
-          <p>Your one-stop solution for all home appliance services.</p>
         </div>
         </>
       );
